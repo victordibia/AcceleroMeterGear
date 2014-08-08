5310b5c529a4c73f106fe2509af624ffd4136e5e
Victor D <chuvidi2003@gmail.com>
2014-08-09 06:53:04 +0800

    Added rotate rate calculation

--- .build/Result Resource Layer/js/main.js
+++ .build/Result Resource Layer/js/main.js
@@ -4,7 +4,7 @@
 //Copyright (c)2014 Dibia Victor, Denvycom
 //Distributed under MIT license
 
-//https://github.com/chuvidi2003/GearSwipeTutorial
+//https://github.com/chuvidi2003/AcceleroMeterGear
 $(window).load(function(){
 
 	//This listens for the back button press
@@ -17,11 +17,23 @@ $(window).load(function(){
 		ax = e.accelerationIncludingGravity.x;
 		ay = -e.accelerationIncludingGravity.y;
 		az = -e.accelerationIncludingGravity.z;
-		document.getElementById("xaccel").innerHTML =  'X : ' +  ax;
-		document.getElementById("yaccel").innerHTML = 'Y : ' + ay;
-		document.getElementById("zaccel").innerHTML = 'Z : ' + az;
-
+		rotx = e.rotationRate.alpha ;
+		roty = e.rotationRate.beta ;
+		rotz = e.rotationRate.gamma ;
+		
+		document.getElementById("xaccel").innerHTML =  'AccX : ' +  ax;
+		document.getElementById("yaccel").innerHTML = 'AccY : ' + ay;
+		document.getElementById("zaccel").innerHTML = 'AccZ : ' + az;
+		
+		document.getElementById("rotx").innerHTML = 'Rot X : ' + rotx ;
+		document.getElementById("roty").innerHTML = 'Rot Y : ' + roty ;
+		document.getElementById("rotz").innerHTML = 'Rot Z : ' + rotz ;
 	}); 
 
+	window.addEventListener("deviceorientation", function(e){
+		//document.getElementById("rotx").innerHTML ='alpha value '+ Math.round(e.alpha);
+		/*betaElem.innerHTML = 'beta value '+ Math.round(e.beta);
+		gammaElem.innerHTML = 'gamma value '+ Math.round(e.gamma);*/
+	}, true);
 
 });
